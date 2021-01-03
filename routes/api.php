e328e4e99eebd63e226a24c168b5466f6a6fc21a
Fernando Frichenbruder <fernando.frich@gmail.com>
2021-01-03 19:04:20 -0300

    Implementação de Login com jwt para API
    Instalado tymon\jwt e criado JwtAuthController e adicionado middleware paras rotas da API

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::get('/products', 'Api\ProductController@apindex');
 
-Route::post('/baixar-produtos', 'Api\SkuController@deduct')->name('deduct');
-Route::post('/adicionar-produtos', 'Api\SkuController@add')->name('add');
+
 
 Route::group(['middleware' => ['auth:api']], function (){
 
         // Route::post('/baixar-produtos', 'Admin\SkuController@deduct')->name('deduct');
         // Route::post('/adicionar-produtos', 'Admin\SkuController@add')->name('add');
         
+});
+
+Route::post('login', 'Api\JwtAuthController@login');
+Route::post('register', 'Api\JwtAuthController@register');
+  
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::post('/baixar-produtos', 'Api\SkuController@deduct')->name('deduct');
+    Route::post('/adicionar-produtos', 'Api\SkuController@add')->name('add');
+    Route::get('logout', 'Api\JwtAuthController@logout');
+    Route::get('products', 'Admin\ProductController@apindex');
 });
